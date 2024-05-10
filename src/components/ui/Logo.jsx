56495c4ce07fd8fe7f6b3c4e2706dea80f26635f
francorinque <francoagustinrinquecorreo@gmail.com>
2024-05-10 11:30:14 -0300

    fix(Logo): fix logo

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,10 +1,6 @@
 import data from '../../mock/data.json';
 
 const Logo = () => {
-  return (
-    <div className='w-[100px] h-[100px] md:w-[150px] md:h-[150px] object-cover flex items-center justify-center'>
-      <img src={data.logo} alt='imagen del logo' className='w-full ' />
-    </div>
-  );
+  return <img src={data.logo} alt='imagen del logo' className='w-[150px]' />;
 };
 export default Logo;
